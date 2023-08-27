fdffb87f6e65c0520f9574ce92e909434450ae04
Mosesomo <moseswamboga4@gmail.com>
2023-08-27 04:25:11 -0700

    sub

--- monty_function3.c
+++ monty_function3.c
@@ -9,12 +9,9 @@
 void func_sub(stack_t **head, unsigned int line_counter)
 {
 	stack_t *temp;
-	int res, len = 0;
+	int result;
 
-	temp = *head;
-	for (; temp != NULL ; len++)
-		temp = temp->next;
-	if (len < 2)
+	if (*head == NULL || (*head)->next == NULL)
 	{
 		fprintf(stderr, "L%d:  can't sub, stack too short\n", line_counter);
 		if (monty.file)
@@ -24,11 +21,11 @@ void func_sub(stack_t **head, unsigned int line_counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
+	result = (*head)->n - (*head)->next;
 	temp = *head;
-	res = temp->next->n - temp->n;
-	temp->next->n = res;
-	*head = temp->next;
+	*head = (*head)->next;
 	free(temp);
+	(*head)->n = result;
 }
 
 /**
